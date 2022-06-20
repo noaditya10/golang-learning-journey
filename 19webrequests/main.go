05047043bd68c86971508be2621ffb03a9c0cbbf
Novi Aditya <noaditya10@gmail.com>
2022-06-20 22:14:49 +0700

    webrequest

---
+++ 19webrequests/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const url = "https://camp404.com"
+
+func main() {
+	fmt.Println("Novi learn web requests")
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Response is of type : %T\n", response)
+
+	defer response.Body.Close() //caller's responsibility to close the connection
+
+	databytes, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	content := string(databytes)
+
+	fmt.Println(content)
+}
